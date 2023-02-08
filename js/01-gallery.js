36a0ab1916f0f2b68543b375aeccd718265e04cb
Y-Savushchyk <115530650+Y-Savushchyk@users.noreply.github.com>
2023-02-08 15:58:30 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,13 +5,13 @@ const galleryListRef = document.createElement("ul")
 galleryListRef.classList.add('.gallery__list')
 galleryListRef.style.listStyle = "none";
 galleryListRef.style.display = "flex"
-for (let i = 0; i < galleryItems.length; i+=1) {
+for (let i = 0; i < galleryItems.length; i += 1) {
     const galleryItemRef = document.createElement('li')
     galleryItemRef.classList.add('.gallery__item')
     const galleryImageRef = document.createElement("img")
     galleryImageRef.classList.add(".gallery__image")
     galleryImageRef.setAttribute("src", galleryItems[i].preview)
-    galleryImageRef.setAttribute("alt",galleryItems[i].description)
+    galleryImageRef.setAttribute("alt", galleryItems[i].description)
     galleryItemRef.append(galleryImageRef)
     galleryListRef.append(galleryItemRef)
 }
