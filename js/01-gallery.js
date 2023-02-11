8fee4a37550b0e70eaf474064ddd5f1db9b71fe8
Y-Savushchyk <savushchky@gmail.com>
2023-02-11 12:11:01 +0200

    insertAdjacentHTML

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,40 +1,33 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const galleryRef = document.querySelector(".gallery");
-const galleryListRef = document.createElement("ul");
-galleryListRef.classList.add("gallery__list");
-galleryListRef.style.listStyle = "none";
-galleryListRef.style.display = "flex";
-galleryListRef.style.flexWrap = "wrap";
-let i = 0;
-for (let i = 0; i < galleryItems.length; i += 1) {
-  const galleryItemRef = document.createElement("li");
-  galleryItemRef.classList.add("gallery__item");
-  galleryItemRef.style.margin = "5px";
-  const galleryLinkRef = document.createElement("a");
-  galleryLinkRef.classList.add("gallery__link");
-  const galleryImageRef = document.createElement("img");
-  galleryImageRef.classList.add("gallery__image");
-  galleryImageRef.setAttribute("src", galleryItems[i].preview);
-  galleryImageRef.setAttribute("alt", galleryItems[i].description);
-  galleryItemRef.style.width = "300px";
-  galleryLinkRef.append(galleryImageRef);
-  galleryItemRef.append(galleryLinkRef);
-  galleryListRef.append(galleryItemRef);
+const galleryDivRef = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryDivRef.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original.value}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-size="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
 }
+const onContainerClick = (e) => {
+  e.preventDefault();
 
-galleryRef.append(galleryListRef);
+  if (e.target.classList.contains("gallery")) return;
+  const size = e.target.dataset.size;
+  const instance = basicLightbox.create(`
+    <img src="${size}"width="800" height="600">`);
 
-galleryRef.addEventListener("click", (e) => {
-  if (e.target.nodeName !== "IMG") {
-    return;
-  } else {
-    e.target.onclick = () => {
-      basicLightbox
-        .create(
-          `<img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg" alt="">`
-        )
-        .show();
-    };
-  }
-});
+  instance.show();
+};
+galleryDivRef.addEventListener("click", onContainerClick);
